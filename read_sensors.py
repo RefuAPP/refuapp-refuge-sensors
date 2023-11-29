58a1504de4bc37ae33bd056d13927145d5c9b27b
alexm3133 <alexandrumateiionut@gmail.com>
2023-11-29 19:16:10 +0100

    actualizando el codigo de la rasberry para que lea los datos solo un sensor de movimiento

--- read_sensors.py
+++ read_sensors.py
@@ -3,94 +3,77 @@ import logging
 import configparser
 import json
 import pandas as pd
-import requests  
-import RPi.GPIO as GPIO  
+import requests
+import RPi.GPIO as GPIO
 
-# Configuracion
+# Configuración
 config = configparser.ConfigParser()
 config.read('config.ini')
 API_URL = config['API']['URL']
-ID_REFUGIO = config['API']['ID_REFUGIO']  
-PASSWORD = config['API']['PASSWORD'] 
-
+ID_REFUGIO = config['API']['ID_REFUGIO']
+PASSWORD = config['API']['PASSWORD']
 
 logging.basicConfig(level=logging.INFO)
 
 class SensorLog:
     def __init__(self):
         self.df = pd.DataFrame(columns=['timestamp', 'status', 'sensor_id'])
-        self.failed_requests = []  # Lista para almacenar solicitudes fallidas
+        self.failed_requests = [] 
 
     def add_entry(self, sensor_id, status):
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
         new_entry = pd.DataFrame([[timestamp, status, sensor_id]], columns=self.df.columns)
         self.df = pd.concat([self.df, new_entry], ignore_index=True)
 
-    def send_to_api(self):
-        data_dict = self.df.to_dict(orient='records')
-        endpoint = f"{API_URL}/sensor/"
+    def send_to_api(self, sensor_entry):
+        data = {
+            "id_refugio": ID_REFUGIO,
+            "password": PASSWORD,
+            "timestamp": sensor_entry['timestamp'],
+            "status": sensor_entry['status'],
+            "sensor_id": sensor_entry['sensor_id']
+        }
 
-        for entry in data_dict:
-            entry['id_refugio'] = ID_REFUGIO
-            entry['password'] = PASSWORD
-
-            try:
-                response = requests.post(endpoint, json=entry)
-                if response.status_code != 200:
-                    logging.warning(f"Error en la API: {response.content}")
-                    self.failed_requests.append(entry)
-            except requests.exceptions.RequestException as e:
-                logging.warning(f"Fallo al conectar con la API: {e}")
-                self.failed_requests.append(entry)
-
-        # Guarda las solicitudes fallidas en un archivo log
+        try:
+            response = requests.post(f"{API_URL}/refugio/", json=data)
+            if response.status_code != 200:
+                logging.warning(f"Error en la API: {response.content}")
+                self.failed_requests.append(data)
+        except requests.exceptions.RequestException as e:
+            logging.warning(f"Fallo al conectar con la API: {e}")
+            self.failed_requests.append(data)
         if self.failed_requests:
             with open("failed_requests.log", "a") as f:
                 for failed_entry in self.failed_requests:
                     f.write(f"{json.dumps(failed_entry)}\n")
 
-        # Intenta reenviar las solicitudes fallidas
-        for failed_entry in self.failed_requests:
-            try:
-                response = requests.post(endpoint, json=failed_entry)
-                if response.status_code == 200:
-                    self.failed_requests.remove(failed_entry)
-            except requests.exceptions.RequestException:
-                pass
-
-def setup_gpio(pins):
+def setup_gpio(pin):
     GPIO.setmode(GPIO.BCM)
-    for pin in pins:
-        GPIO.setup(pin, GPIO.IN)
-
+    GPIO.setup(pin, GPIO.IN)
+    
 def read_sensor(pin):
     return GPIO.input(pin)
 
-
 def main():
-    GPIO_PINS = [20, 21]
+    GPIO_PIN = 21  
     sensor_log = SensorLog()
-    setup_gpio(GPIO_PINS)
-    
-    last_sensor_values = {pin: None for pin in GPIO_PINS} 
+    setup_gpio(GPIO_PIN)
+
+    last_sensor_value = None
 
     try:
         while True:
-            for i, pin in enumerate(GPIO_PINS):
-                sensor_value = read_sensor(pin)
+            sensor_value = read_sensor(GPIO_PIN)
 
-                if sensor_value != last_sensor_values[pin]:
-                    last_sensor_values[pin] = sensor_value  
+            if sensor_value != last_sensor_value:
+                last_sensor_value = sensor_value
 
-                    if sensor_value == 1:
-                        logging.info(f"Sensor {i+1}: Ningún obstáculo detectado")
-                        sensor_log.add_entry(i+1, "No obstacle")
-                    else:
-                        logging.warning(f"Sensor {i+1}: Obstáculo detectado")
-                        sensor_log.add_entry(i+1, "Obstacle")
+                status = "No obstacle" if sensor_value == 1 else "Obstacle"
+                logging.info(f"Sensor: {status}")
+                sensor_log.add_entry(GPIO_PIN, status)
 
-                    sensor_log.send_to_api()  
-                    sensor_log.df = sensor_log.df.iloc[0:0] 
+                sensor_log.send_to_api(sensor_log.df.iloc[-1].to_dict())
+                sensor_log.df = sensor_log.df.iloc[0:0]
 
             time.sleep(1)
 
@@ -101,4 +84,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
